e09f183ee06b163539a6dbfe29d38df97c4e734e
Aliaksiej Homza <aliaksei.homza@gmail.com>
2016-02-16 23:11:35 +0300

    Initial app structure

---
+++ assets/App.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+
+  }
+</script>
+
+<template>
+  <h1>Hello, world</h1>
+</template>
+
+<style lang="stylus">
+
+</style>
+
+<style lang="stylus" scoped>
+
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel'}
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+      {test: /\.vue$/, loader: 'vue'}
     ]
   }
 }
